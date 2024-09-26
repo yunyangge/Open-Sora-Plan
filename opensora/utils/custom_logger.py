4c4808e8b2be0f314eb7bb0c651f2f1038bff29d
cxh0519 <931736813@qq.com>
2024-09-26 18:21:45 +0800

    update logs

---
+++ opensora/utils/custom_logger.py
@@ -0,0 +1,34 @@
+import logging
+import colorlog
+import accelerate.logging
+
+def get_logger(name, level=logging.DEBUG):
+
+    logger = accelerate.logging.get_logger(name)
+    logger.logger.setLevel(level)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(level)
+
+    color_formatter = colorlog.ColoredFormatter(
+        '%(yellow)s[%(asctime)s] %(log_color)s[%(levelname)s] %(green)s[%(name)s] %(log_color)s%(message)s',
+        datefmt=None,
+    	reset=True,
+        log_colors={
+            'DEBUG': 'blue',
+            'INFO': 'cyan',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        },
+        secondary_log_colors={},
+	    style='%'
+    )
+
+    console_handler.setFormatter(color_formatter)
+
+    for handler in logger.logger.handlers:
+        logger.logger.removeHandler(handler)
+
+    logger.logger.addHandler(console_handler)
+    return logger
