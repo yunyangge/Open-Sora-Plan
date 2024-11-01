21fe21d0a42fcbaae50699524e72d7283c8c2426
yunyangge <gyy23233.163.com>
2024-11-01 16:36:47 +0800

    mmdit npu

--- scripts/text_condition/npu/sample_t2v_v1_5.sh
+++ scripts/text_condition/npu/sample_t2v_v1_5.sh
@@ -3,7 +3,7 @@ export TRANSFORMERS_OFFLINE=1
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nnodes=1 --nproc_per_node 8 --master_port 29512 \
     -m opensora.sample.sample \
-    --model_path /home/save_dir/runs/t2v_1_5_dit_bs16x16x8_lr1e-4_256x256_192x192_new6b/checkpoint-45000/model_ema \
+    --model_path /home/save_dir/runs/t2v_1_5_dit_bs16x32x8_lr1e-4_256x256_192x192_new6b/checkpoint-6000/model \
     --version v1_5 \
     --num_frames 1 \
     --height 256 \
@@ -14,10 +14,10 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nnodes=1 --nproc_per_node 8 --ma
     --text_prompt examples/sora.txt \
     --ae WFVAEModel_D32_8x8x8 \
     --ae_path "/home/save_dir/lzj/Middle888" \
-    --save_img_path "./t2v_1_5_45k_step100_ema" \
+    --save_img_path "./t2v_new_script_6k" \
     --fps 18 \
     --guidance_scale 7.0 \
-    --num_sampling_steps 100 \
+    --num_sampling_steps 28 \
     --max_sequence_length 512 \
     --sample_method FlowMatchEulerDiscrete \
     --seed 1234 \
